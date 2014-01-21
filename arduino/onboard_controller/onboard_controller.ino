3dd34b878986f0f60bd83775221bfaafb816e0b5
Conor Cary <conor.cary@gmail.com>
2014-01-20 23:10:10 -0500

    First working event driven comms.

--- arduino/onboard_controller/onboard_controller.ino
+++ arduino/onboard_controller/onboard_controller.ino
@@ -18,10 +18,10 @@ const byte CHECK_BYTE = 243;
 const int BAUD_RATE = 9600;
 
 void setup() {
-  
+
   // Initialize serial port.
   Serial.begin( BAUD_RATE );
-  
+
   // Initialize thruster servos.
   port_thruster_servo.attach( PORT_THRUSTER_SERVO_PIN, 400, 2200 );
   starboard_thruster_servo.attach( STARBOARD_THRUSTER_SERVO_PIN, 400, 2200);
@@ -31,21 +31,13 @@ void loop(){
 
   // Send ready status to control station.
   Serial.write( CHECK_BYTE );
-
-  // Wait for incoming data packet.
-  while( Serial.available() < 5 ){
-  }
-
-  // Read incoming data packet.
-  port_thruster_motor_value = Serial.read();
-  port_thruster_servo_value = Serial.read();
-  starboard_thruster_motor_value = Serial.read();
-  starboard_thruster_servo_value = Serial.read();
-  aft_thruster_motor_value = Serial.read();
+  Serial.write( '#' );
 
   // Send thruster motor values back for debugging.
+  Serial.write( byte(244) );
   Serial.write( port_thruster_motor_value );
   Serial.write( starboard_thruster_motor_value );
+  Serial.write( '#' );
 
   // Set thruster motor speeds.
   analogWrite( PORT_THRUSTER_MOTOR_PIN, port_thruster_motor_value );
@@ -54,11 +46,20 @@ void loop(){
   // Set servo positions according to the scaled values.
   port_thruster_servo.write( port_thruster_servo_value ); 
   starboard_thruster_servo.write( starboard_thruster_servo_value );
-  
+
   // Allow time for each servo operation to complete.
   delay( 15 );                           
 }
 
+void serialEvent() {
+  
+  // Read incoming data packet.
+  port_thruster_motor_value = Serial.read();
+  port_thruster_servo_value = Serial.read();
+  starboard_thruster_motor_value = Serial.read();
+  starboard_thruster_servo_value = Serial.read();
+  aft_thruster_motor_value = Serial.read();
+}
 
 
 
--- processing/control_station/SerialThread.pde
+++ processing/control_station/SerialThread.pde
@@ -2,11 +2,12 @@ class SerialThread extends Thread {
 
   private final int BAUD_RATE = 9600;
   private boolean running = false;
-  private Serial serial;
+  public Serial serial;
 
   // Constructor
   SerialThread( PApplet main_function ) {
-    serial = new Serial( main_function, Serial.list()[0], BAUD_RATE ); 
+    serial = new Serial( main_function, Serial.list()[0], BAUD_RATE );
+    serial.bufferUntil('#');
     this.start();
   }
 
@@ -19,33 +20,35 @@ class SerialThread extends Thread {
   // This gets triggered by start().
   void run() {
     while ( running ) {
+    }
+  }
 
-      // Wait for serial activity.
-      while ( serial.available () < 1 ) {
-      }
-
-      // Test check byte.
-      check = serial.read();
-      // Send current status (model?).
-      if ( check == 243 ) {
-        serial.write(left_motor);
-        serial.write(left_servo);
-        serial.write(right_motor);
-        serial.write(right_servo);
-        serial.write(Z_motor);
-      }
-      // Wait for serial activity.
-      while ( serial.available () < 2 ) {
-      }
+  void event() {
+    int check = serial.read();
+    if ( check == 243 ) {
+      serial.read();
+      serial.write(left_motor);
+      serial.write(left_servo);
+      serial.write(right_motor);
+      serial.write(right_servo);
+      serial.write(Z_motor);
+    }
+    if (check == 244) {
       // Print debugging info.
       println( serial.read() + "    " + serial.read() );
-      // Reset check.
-      check = 0;
+      serial.read();
     }
+    // Reset check.
+    check = 0;
   }
 
   void quit() {
     running = false;
+    serial.stop();
   }
+}
+
+void serialEvent(Serial serial) {
+  serialThread.event();
 }
 
