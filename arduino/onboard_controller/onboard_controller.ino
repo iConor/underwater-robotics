42efe46f36e428888c44ad9da2221b2588000a7c
Conor Cary <cary.42@osu.edu>
2014-01-27 02:02:19 -0500

    Switched motors to Mega's Serial1.

--- arduino/onboard_controller/onboard_controller.ino
+++ arduino/onboard_controller/onboard_controller.ino
@@ -4,25 +4,24 @@
 const byte CHECK_BYTE = 243;
 const int BAUD_RATE = 9600;
 
-const int PORT_THRUSTER_MOTOR_PIN = 5;
-const int STARBOARD_THRUSTER_MOTOR_PIN = 6;
-//const int AFT_THRUSTER_MOTOR_PIN = 7;
-const int PORT_THRUSTER_SERVO_PIN = 9;
-const int STARBOARD_THRUSTER_SERVO_PIN = 10;
+const int PORT_THRUSTER_SERVO_PIN = 10;
+const int STARBOARD_THRUSTER_SERVO_PIN = 11;
 
 const int SERVO_MIN = 400;
 const int SERVO_MAX = 2200;
 
+const int THRUSTER_ADDRESS = 128;
+const int PORT_THRUSTER = 2;
+const int STARBOARD_THRUSTER = 1;
+
+int port_thruster_servo_value;
+int starboard_thruster_servo_value;
 int port_thruster_motor_value;
 int starboard_thruster_motor_value;
 int aft_thruster_motor_value;
-int port_thruster_servo_value;
-int starboard_thruster_servo_value;
 
 Servo port_thruster_servo;
 Servo starboard_thruster_servo;
-
-SoftwareSerial motorSerial(10,11);
 
 void setup() {
 
@@ -30,7 +29,7 @@ void setup() {
   Serial.begin( BAUD_RATE );
 
   // Initialize motor port.
-  motorSerial.begin( BAUD_RATE );
+  Serial1.begin( BAUD_RATE );
 
   // Initialize thruster servos.
   port_thruster_servo.attach( PORT_THRUSTER_SERVO_PIN, SERVO_MIN, SERVO_MAX );
@@ -58,8 +57,8 @@ void loop() {
   Serial.write( starboard_thruster_motor_value );
 
   // Set thruster motor speeds.
-  motorControl( 128, 1, port_thruster_motor_value );
-  motorControl( 128, 2, starboard_thruster_motor_value );
+  motorControl( THRUSTER_ADDRESS, PORT_THRUSTER, port_thruster_motor_value );
+  motorControl( THRUSTER_ADDRESS, STARBOARD_THRUSTER, starboard_thruster_motor_value );
 
   // Set servo positions according to the scaled values.
   port_thruster_servo.write( port_thruster_servo_value ); 
@@ -70,34 +69,36 @@ void loop() {
 }
 
 void motorControl( int address_, int motor_, int power ) {
+  byte thing1_;
   int speed_;
-  motorSerial.write( address_ );
+  byte thing2_;
   if( motor_ == 1 ) {
     if( power >= 128 ) {
+      thing1_ = ( byte ) 0;
       speed_ = power - 127;
-      motorSerial.write( ( byte ) 0 );
-      motorSerial.write( speed_ );
-      motorSerial.write( ( speed_ + address_ ) & 127 );
+      thing2_ = ( speed_ + address_ ) & 127;
     } 
     else {
+      thing1_ = 1;
       speed_ = 127 - power;
-      motorSerial.write( 1 );
-      motorSerial.write( speed_ );
-      motorSerial.write( ( speed_ + address_ + 1 ) & 127 );
+      thing2_ = ( speed_ + address_ + 1 ) & 127;
     }
   }
   else { // motor_ == 2
     if( power >= 128 ) {
+      thing1_ = 4;
       speed_ = power - 127;
-      motorSerial.write( 4 );
-      motorSerial.write( speed_ );
-      motorSerial.write( ( speed_ + address_ + 4 ) & 127) ;
+      thing2_ = ( speed_ + address_ + 4 ) & 127;
     }
     else {
+      thing1_ = 5;
       speed_ = 127 - power;
-      motorSerial.write( 5 );
-      motorSerial.write( speed_ );
-      motorSerial.write( ( speed_ + address_ + 5 ) & 127 );
+      thing2_ = ( speed_ + address_ + 5 ) & 127;
     }
   }
+  Serial1.write( address_ );
+  Serial1.write( thing1_ );
+  Serial1.write( speed_ );
+  Serial1.write( thing2_ );
 }
+
